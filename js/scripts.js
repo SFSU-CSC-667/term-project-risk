3a1071d5a733fd258c36a9b7e5feb1c7292fc2ae
sapansanu <sapansanu@gmail.com>
2016-12-02 20:18:20 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,302 @@
+function middleEast() {
+	console.log("Middle East");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Middle East");
+		document.getElementById('middleEastText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function india() {
+	console.log("India");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in India");
+		document.getElementById('indiaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function afghanistan() {
+	console.log("Afghanistan");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Afghanistan");
+		document.getElementById('afghanistanText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function china() {
+	console.log("China");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in China");
+		document.getElementById('chinaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function ural() {
+	console.log("Ural");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Ural");
+		document.getElementById('uralText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function siberia() {
+	console.log("Siberia");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Siberia");
+		document.getElementById('siberiaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function mongolia() {
+	console.log("Mongolia");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Mongolia");
+		document.getElementById('mongoliaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function irkutsk() {
+	console.log("Irkutsk");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Irkutsk");
+		document.getElementById('irkutskText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function japan() {
+	console.log("Japan");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in Japan");
+		document.getElementById('japanText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function southAfrica() {
+	console.log("South Africa");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in South Africa");
+		document.getElementById('southAfricaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function northAfrica() {
+	console.log("North Africa");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in North Africa");
+		document.getElementById('northAfricaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function eastAfrica() {
+	console.log("East Africa");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in eastAfrica");
+		document.getElementById('eastAfricaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function congo() {
+	console.log("Congo");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in congo");
+		document.getElementById('congoText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function egypt() {
+	console.log("Egypt");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in egypt");
+		document.getElementById('egyptText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function brazil() {
+	console.log("Brazil");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in India");
+		document.getElementById('indiaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function argentina() {
+	console.log("Argentina");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in argentina");
+		document.getElementById('argentinaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function westernUS() {
+	console.log("Western US");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in westernUS");
+		document.getElementById('westernUSText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function alberta() {
+	console.log("Alberta");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in India");
+		document.getElementById('indiaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function westernAustralia() {
+	console.log("Western Australia");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in westernAustralia");
+		document.getElementById('westernAustraliaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function easternAustralia() {
+	console.log("Eastern Australia");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in easternAustralia");
+		document.getElementById('easternAustraliaext').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function madagascar() {
+	console.log("Madagascar");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in madagascar");
+		document.getElementById('madagascarText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function peru() {
+	console.log("Peru");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in peru");
+		document.getElementById('peruText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function venezuela() {
+	console.log("Venezuela");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in venezuela");
+		document.getElementById('venezuelaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function indonesia() {
+	console.log("Indonesia");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in indonesia");
+		document.getElementById('indonesiaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function newGuinea() {
+	console.log("New Guinea");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in newGuinea");
+		document.getElementById('newGuineaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function kamchatka() {
+	console.log("Kamchatka");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in kamchatka");
+		document.getElementById('kamchatkaText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function yakutsk() {
+	console.log("Yakutsk");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in yakutsk");
+		document.getElementById('yakutskText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+function slam() {
+	console.log("Slam");
+	modal();
+	document.getElementById('deploy').onclick = function() { 
+		document.getElementById('myModal').style.display = "none";
+		console.log("Deployed "+document.getElementById('value').value+" in slam");
+		document.getElementById('slamText').textContent = document.getElementById('value').value;
+		document.getElementById('value').value = 0;	
+	}
+}
+
+
+function modal() {
+	var currentVal;
+	var modal = document.getElementById('myModal');
+	modal.style.display = "block";
+	document.getElementsByClassName("close")[0].onclick = function() { 
+		modal.style.display = "none";
+		document.getElementById('value').value = 0;	
+	}
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	        //document.getElementById('value').value = 0;	
+	    }
+	}
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+});
+	
