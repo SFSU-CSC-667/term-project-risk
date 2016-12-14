7b894195690cd9d29b0d31f1f2fb2286618a0651
Paul Klein <paulleoklein@gmail.com>
2016-12-14 12:11:08 -0800

    Fixing current value of territories in the model text box

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,27 +1,27 @@
 function territoryClicked(name) {
 	console.log(name);
-	modal();
+	modal(name);
 	document.getElementById('deploy').onclick = function() { 
 		document.getElementById('myModal').style.display = "none";
 		console.log("Deployed "+document.getElementById('value').value+" in " + name);
 		document.getElementById(name + 'Text').textContent = document.getElementById('value').value;
-		document.getElementById('value').value = 0;	
 	}
 }
 
 
-function modal() {
-	var currentVal;
+function modal(name) {
+	//Assigns the current value to the input, or zero if it isn't set
+	document.getElementById('value').value = document.getElementById(name + "Text").textContent == "" ? 
+		0 : document.getElementById(name + "Text").textContent;
 	var modal = document.getElementById('myModal');
 	modal.style.display = "block";
+	//Hides model when you click away or click the close button
 	document.getElementsByClassName("close")[0].onclick = function() { 
 		modal.style.display = "none";
-		document.getElementById('value').value = 0;	
 	}
 	window.onclick = function(event) {
 	    if (event.target == modal) {
 	        modal.style.display = "none";
-	        //document.getElementById('value').value = 0;	
 	    }
 	}
 }
